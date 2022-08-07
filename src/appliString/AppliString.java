32ad09b74e21f5443f0e6145026c6d56c7355da6
Abdourahamane <amdroudjae@gmail.com>
2022-08-07 13:55:37 +0200

    Création de l'interface graphique

---
+++ src/appliString/AppliString.java
@@ -0,0 +1,43 @@
+package appliString;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class AppliString {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AppliString window = new AppliString();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AppliString() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
